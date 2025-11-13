753bba2cd9832c063f4e5a7eddd511b1452ff7ea
Angel <angelalbistro@gmail.com>
2025-11-12 23:41:44 -0300

    agregados CRUD, controllers, routes modificados, etc

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-import express from "express" 
+import http from "http";
+import { Server } from "socket.io";
+import express from "express";
+import handlebars from "express-handlebars";
 
-import products from "./routes/products.js"; 
-import carts from "./routes/carts.js";
+//rutas
+import productsRouter from "./routes/products.route.js";
+import cartsRouter from "./routes/carts.route.js";
+import rootRouter from "./routes/root.route.js";
 
 
-//creo mi servidor
-const servidor = express()
-
+//configuracion
 const PORT = 8080
 
+//express p/logica
+//deja de ser el servidor real y abrir el puerto con la implementacion de websocket
+const app = express()
+
+//Este es el servidor http que responde solicitudes que vengan de http://localhost:5000, lo hace con la constante de app (arriba)
+const servidor = http.createServer(app);
+
+
+//Creo un nuevo servidor websocket
+//Este es el servidor de WebSocket que responde solicitudes que vengan de : ws://localhost:5000
+const servidorWS = new Server(servidor)
+
+
+
+
+servidorWS.on("connection", (socket) => {
+    console.log("Nuevo cliente conectado");
+});
+
+
+// Motor de vista handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+
+
 //Middleware
-servidor.use (express.json());
-servidor.use("/api/products", products);
-servidor.use("/api/carts", carts);
+app.use(express.json());
+//para poder leer formularios
+app.use(express.urlencoded({ extended: true }));
+
+//esto significa que si me hacen un pedido con metodo GET y la ruta es /index.css (p/ej) busco el archivo en la carpeta public, si existe se devuelve, sino sera 404
+//adonde queres que express busque el archivo css, tipografia, etc que necesite el front
+app.use(express.static("public"));
+
+
+//rutas
+app.use("/", rootRouter);
+app.use("/products", productsRouter);
+app.use("/carts", cartsRouter);
+
+// manejo de errores global
+app.use((err, req, res, next) => {
+    console.log(err); 
+    res.status(500).send("Error interno del servidor");
+});
+
 
 servidor.listen(PORT, ()=>{
     console.log(`Servidor corriendo en puerto ${PORT}`)
